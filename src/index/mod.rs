ca4e251ad1e81736b12cf3d971aec8079f6a647f
Sardorbek Pulatov <sardo@hey.com>
2023-05-08 13:23:49 +0200

    feat(index): Add index operation endpoints

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -12,6 +12,7 @@ use crate::error::Error;
 
 pub mod settings;
 pub mod task;
+pub mod operation;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -303,11 +304,11 @@ pub struct SearchQuery {
     #[builder(setter(into))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// [https://www.algolia.com/doc/api-reference/api-parameters/ignorePlurals/](https://www.algolia.com/doc/api-reference/api-parameters/ignorePlurals/)
-    ignore_plurals: Option<crate::settings::IgnorePlurals>,
+    ignore_plurals: Option<settings::IgnorePlurals>,
     #[builder(setter(into))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// [https://www.algolia.com/doc/api-reference/api-parameters/removeStopWords/](https://www.algolia.com/doc/api-reference/api-parameters/removeStopWords/)
-    remove_stop_words: Option<crate::settings::IgnorePlurals>,
+    remove_stop_words: Option<settings::IgnorePlurals>,
     #[builder(setter(into))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// [https://www.algolia.com/doc/api-reference/api-parameters/queryLanguages/](https://www.algolia.com/doc/api-reference/api-parameters/queryLanguages/)
@@ -337,11 +338,11 @@ pub struct SearchQuery {
     #[builder(setter(into))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// [https://www.algolia.com/doc/api-reference/api-parameters/exactOnSingleWordQuery/](https://www.algolia.com/doc/api-reference/api-parameters/exactOnSingleWordQuery/)
-    exact_on_single_word_query: Option<crate::settings::ExactOnSingleWordQuery>,
+    exact_on_single_word_query: Option<settings::ExactOnSingleWordQuery>,
     #[builder(setter(into))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// [https://www.algolia.com/doc/api-reference/api-parameters/alternativesAsExact/](https://www.algolia.com/doc/api-reference/api-parameters/alternativesAsExact/)
-    alternatives_as_exact: Option<crate::settings::AlternativesAsExact>,
+    alternatives_as_exact: Option<settings::AlternativesAsExact>,
 
     // query-rules
     #[builder(setter(into))]
@@ -395,7 +396,7 @@ pub struct SearchQuery {
     #[builder(setter(into))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// [https://www.algolia.com/doc/api-reference/api-parameters/minProximity/](https://www.algolia.com/doc/api-reference/api-parameters/minProximity/)
-    min_proximity: Option<crate::settings::MinProximity>,
+    min_proximity: Option<settings::MinProximity>,
     #[builder(setter(into))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// [https://www.algolia.com/doc/api-reference/api-parameters/responseFields/](https://www.algolia.com/doc/api-reference/api-parameters/responseFields/)
@@ -462,7 +463,7 @@ struct BatchedOperation<T> {
 }
 
 #[derive(Debug, Deserialize)]
-pub struct BatchedOperatioResult {
+pub struct BatchedOperationResult {
     #[serde(rename = "taskID")]
     pub task_id: u64,
     #[serde(rename = "objectIDs")]
@@ -611,7 +612,7 @@ impl<T: DeserializeOwned + Serialize> Index<T> {
     /// #   Ok(())
     /// # }
     /// ```
-    pub async fn add_objects(&self, objects: &[&T]) -> Result<BatchedOperatioResult, Error> {
+    pub async fn add_objects(&self, objects: &[&T]) -> Result<BatchedOperationResult, Error> {
         let uri = format!("{}/indexes/{}/batch", self.base_url, self.index_name);
         let requests = objects.iter().fold(vec![], |mut acc, x| {
             acc.push(BatchedOperationItem {
@@ -681,7 +682,7 @@ impl<T: DeserializeOwned + Serialize> Index<T> {
     /// #   Ok(())
     /// # }
     /// ```
-    pub async fn update_objects(&self, objects: &[&T]) -> Result<BatchedOperatioResult, Error> {
+    pub async fn update_objects(&self, objects: &[&T]) -> Result<BatchedOperationResult, Error> {
         let uri = format!("{}/indexes/{}/batch", self.base_url, self.index_name);
         let requests = objects.iter().fold(vec![], |mut acc, x| {
             acc.push(BatchedOperationItem {
@@ -711,7 +712,7 @@ impl<T: DeserializeOwned + Serialize> Index<T> {
     /// # async fn main() -> Result<(), Box<Error>> {
     /// #   let index = Client::default().init_index::<User>("users");
     /// #   let object_1 = User { object_id: "test".into(), };
-    /// index.delete_object(&object_1.object_id).await?;
+    /// #   index.delete_object(&object_1.object_id).await?;
     /// #   Ok(())
     /// # }
     /// ```
@@ -844,7 +845,7 @@ impl<T: DeserializeOwned + Serialize> Index<T> {
     /// # #[tokio::main]
     /// # async fn main() -> Result<(), Box<Error>> {
     /// #   let index = Client::default().init_index::<User>("users");
-    /// let task = index.get_task_status(123);
+    /// #   let task = index.get_task_status(123);
     /// #   Ok(())
     /// # }
     /// ```
@@ -858,6 +859,70 @@ impl<T: DeserializeOwned + Serialize> Index<T> {
             .headers(self.get_headers())
             .send()
             .await?
+            .json()
+            .await
+            .map_err(|e| e.into())
+    }
+
+    /// Delete an index.
+    /// ```no_run
+    /// # #[macro_use] extern crate serde_derive;
+    /// # use algoliasearch::{Error, Client, SearchQueryBuilder};
+    /// # #[derive(Serialize, Deserialize)]
+    /// # struct User { object_id: String, }
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), Box<Error>> {
+    /// #   let index = Client::default().init_index::<User>("users");    ///
+    /// #   index.delete_index().await?;
+    /// #   Ok(())
+    /// # }
+    /// ```
+    pub async fn delete_index(&self) -> Result<operation::IndexOperationResult, Error> {
+        let uri = format!(
+            "{}/indexes/{}",
+            self.base_url, self.index_name
+        );
+        Client::new()
+            .delete(&uri)
+            .headers(self.get_headers())
+            .send()
+            .await
+            .json()
+            .await
+            .map_err(|e| e.into())
+    }
+
+    /// Copy or move an existing index.
+    /// ```no_run
+    /// # #[macro_use] extern crate serde_derive;
+    /// # use algoliasearch::{Error, Client, SearchQueryBuilder, index};
+    /// # #[derive(Serialize, Deserialize)]
+    /// # struct User { object_id: String, }
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), Box<Error>> {
+    /// #   let index = Client::default().init_index::<User>("users");
+    /// #   // Rename the index
+    /// #   let operation = index::operation::IndexOperationResponse{
+    /// #                     operation: "move".to_string(),
+    /// #                     destination: "user2".to_string(),
+    /// #                     scope: None,
+    /// #   };
+    /// #   index.index_operation(operation).await?;
+    /// #   Ok(())
+    /// # }
+    /// ```
+    pub async fn index_operation(&self, operation: operation::IndexOperationResponse) -> Result<operation::IndexOperationResult, Error> {
+        let uri = format!(
+            "{}/indexes/{}/operation",
+            self.base_url, self.index_name
+        );
+
+        Client::new()
+            .post(&uri)
+            .headers(self.get_headers())
+            .json(&operation)
+            .send()
+            .await
             .json()
             .await
             .map_err(|e| e.into())
